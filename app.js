e8d23ad5c3180c0831405494119e1c6b4ff4aa56
Carlos Santos <chvs1524@gmail.com>
2021-05-06 12:13:27 -0300

    Mini back-end para uso do Twitter API

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const Twitter = require('twit');
+
+const app = express();
+const client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+ 
+app.use(require('cors')());
+app.use(require('body-parser').json());
+
+//See timeline of my tweet
+app.get('/home_timeline', (req, res) => {
+  const params = { tweet_mode: 'extended', count: 10 };
+ 
+  client
+    .get(`statuses/home_timeline`, params)
+    .then(timeline => {
+       
+      res.send(timeline);
+    })
+    .catch(error => {
+    res.send(error);
+  }); 
+});
+
+//Get tweets of user
+app.get('/user_timeline', (req, res) => {
+  const params = { tweet_mode: 'extended', exclude_replies: true, count: 10, screen_name: req.query.user };
+  
+  client
+    .get(`statuses/user_timeline`, params)
+    .then(timeline => {
+       
+      res.send(timeline);
+    })
+    .catch(error => {
+    res.send(error);
+  });
+});
+
+//Post tweets
+app.post('/post_tweet', (req, res) => {
+  const tweet = req.body;
+
+    client
+      .post(`statuses/update`, tweet)
+      .then(tweeting => {
+         
+        res.send(tweeting);
+      })
+ 
+     .catch(error => {
+      res.send(error);
+    });
+});
+
+app.listen(3000, () => console.log('Server running'));
+
+module.exports = app;
